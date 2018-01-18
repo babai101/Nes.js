88009c4782459e0a5669489ee929f92c08a6f6c5
Soumyadeep Chanda <deep.fatality@gmail.com>
2018-01-18 05:58:47 +0000

    Refactored for mapper support

--- nesDebug.js
+++ nesDebug.js
@@ -1,48 +1,40 @@
+/*global $ mmu cpu apu ppu display iNES mapper*/
 $(document).ready(function() {
-    var ines;
-    var CPU;
-    var PPU;
-    var MMU;
-    var APU;
-    var mainDisplay;
-    var oamDisplay;
     var nmTbl0Display;
     var nmTbl1Display;
     var nmTbl2Display;
     var nmTbl3Display;
     // var traceLimitOption;
     var isPaused = false;
-    ines = new iNES();
+    
     // var isDebugging = true;
     var initGame = function(romContent) {
+        this.ines = new iNES();
         this.opcodes = new Uint8Array(romContent);
-        mainDisplay = new display(document.getElementById('nesCanvas'));
-        oamDisplay = new display(document.getElementById('oamCanvas'));
+        this.mainDisplay = new display(document.getElementById('nesCanvas'));
+        this.ines = new iNES(this);
+        this.Mapper = new mapper(this);
+        this.PPU = new ppu(this);
+        this.MMU = new mmu(this);
+        this.CPU = new cpu(this);
+        this.APU = new apu(this);
+        this.MMU.OAMInit();
+        this.PPU.initScreenBuffer();
+        this.ines.parseRom(this.opcodes);
+        this.MMU.nameTableMirroring = this.ines.mirroring;
+        this.PPU.nameTableMirroring = this.ines.mirroring;
+        this.CPU.reset();
+        this.oamDisplay = new display(document.getElementById('oamCanvas'));
         nmTbl0Display = new display(document.getElementById('nmTbl0Canvas'));
         nmTbl1Display = new display(document.getElementById('nmTbl1Canvas'));
         nmTbl2Display = new display(document.getElementById('nmTbl2Canvas'));
         nmTbl3Display = new display(document.getElementById('nmTbl3Canvas'));
-        PPU = new ppu(mainDisplay);
-        MMU = new mmu(PPU);
-        CPU = new cpu(MMU, PPU);
-        APU = new apu(MMU);
-        MMU.OAMInit();
-        PPU.initScreenBuffer();
         nmTbl0Display.initScreenBuffer();
         nmTbl1Display.initScreenBuffer();
         nmTbl2Display.initScreenBuffer();
         nmTbl3Display.initScreenBuffer();
-        // if (isDebugging) {
-        oamDisplay.initScreenBuffer();
-        // }
-        ines.load(this.opcodes, MMU);
-        MMU.nameTableMirroring = ines.mirroring;
-        PPU.nameTableMirroring = ines.mirroring;
-        CPU.reset();
-        MMU.copyCHRToGrid();
-        MMU.copyBGRCHRToGrid();
-        mainDisplay.screenReset();
-
+        this.oamDisplay.initScreenBuffer();
+        this.mainDisplay.screenReset();
     };
     var showNameTables = function() {
         var tempAddr = [0x2000, 0x2400, 0x2800, 0x2C00];
@@ -51,19 +43,19 @@ $(document).ready(function() {
             var nametable = [];
             var attrtable = [];
             for (var i = tempAddr[x]; i < (tempAddr[x] + 960); i++)
-                nametable.push(MMU.ppuMem[i]);
+                nametable.push(this.MMU.ppuMem[i]);
             for (i = tempAddr[x] + 960; i < (tempAddr[x] + 960 + 64); i++)
-                attrtable.push(MMU.ppuMem[i]);
+                attrtable.push(this.MMU.ppuMem[i]);
             renderNameTable(nametable, attrtable, nmTblArr[x]);
         }
     };
 
-    var startdownFunction = function() {
-        MMU.startBtnState = true;
-    };
-    var startupFunction = function() {
-        MMU.startBtnState = false;
-    };
+    // var startdownFunction = function() {
+    //     MMU.startBtnState = true;
+    // };
+    // var startupFunction = function() {
+    //     MMU.startBtnState = false;
+    // };
     var renderNameTable = function(nameTable, attrtable, nmTblDisplay) {
         nmTblDisplay.screenReset();
         var paletteNum = 0;
@@ -77,18 +69,10 @@ $(document).ready(function() {
         for (curTileY = 0; curTileY < 30; curTileY++) {
             for (curTileX = 0; curTileX < 32; curTileX++) {
                 var tileToDraw = nameTable[curTileX + curTileY * 32];
-                if (PPU.backgroundPatTblAddr == 'left') {
-                    tileToDraw = PPU.CHRGrid[tileToDraw];
-                }
-                else if (PPU.backgroundPatTblAddr == 'right') {
-                    tileToDraw = PPU.BGRCHRGrid[tileToDraw];
-                }
-                // tileToDraw = PPU.backgroundPatTblAddr[tileToDraw];
+                tileToDraw = this.Mapper.getCHRGrid(this.PPU.backgroundPatTblAddr, tileToDraw);
                 var curAttrX = Math.floor(curTileX / 4);
                 var curAttrY = Math.floor(curTileY / 4);
-
                 var attrByte = attrtable[curAttrX + curAttrY * 8];
-                // var attrByte = attrtable[curAttrX + curAttrY * 8];
                 //Top left of 2x2 tile
                 if (((curTileX % 4 == 0) || (curTileX % 4 == 1)) && ((curTileY % 4 == 0) || (curTileY % 4 == 1))) {
                     // paletteNum = attrByte >> 6;
@@ -117,13 +101,13 @@ $(document).ready(function() {
                     for (var curX = 0; curX < 8; curX++) {
                         //Pallete logic broken down
                         if (tileToDraw[curY][curX] == 0) { //backdrop color
-                            pixelColor = PPU.paletteColors[PPU.palette[0]];
+                            pixelColor = this.PPU.paletteColors[this.PPU.palette[0]];
                             screenPixel = ((curTileX * 8) + curX) + ((curTileY * 8) + curY) * 256;
                             //this.screenBuffer[screenPixel] = pixelColor;
                             nmTblDisplay.updateBuffer(screenPixel, pixelColor);
                         }
                         else {
-                            pixelColor = PPU.paletteColors[PPU.palette[paletteNum * 4 + tileToDraw[curY][curX]]];
+                            pixelColor = this.PPU.paletteColors[this.PPU.palette[paletteNum * 4 + tileToDraw[curY][curX]]];
                             screenPixel = ((curTileX * 8) + curX) + ((curTileY * 8) + curY) * 256;
                             //this.screenBuffer[screenPixel] = pixelColor;
                             nmTblDisplay.updateBuffer(screenPixel, pixelColor);
@@ -136,9 +120,9 @@ $(document).ready(function() {
     };
 
     var renderScreen = function() {
-        mainDisplay.updateCanvas();
+        this.mainDisplay.updateCanvas();
         drawOAM();
-        oamDisplay.updateCanvas();
+        this.oamDisplay.updateCanvas();
         showNameTables();
     };
 
@@ -148,8 +132,8 @@ $(document).ready(function() {
             var temp = Math.floor(i / 4);
             //temp = Math.floor(temp / 8); //get the Y co-ordinate 
             tempOAM[i] = Math.floor(temp / 8); //Y attribute 
-            tempOAM[i + 1] = MMU.OAM[i + 1]; //tile number
-            tempOAM[i + 2] = MMU.OAM[i + 2]; //attribute 
+            tempOAM[i + 1] = this.MMU.OAM[i + 1]; //tile number
+            tempOAM[i + 2] = this.MMU.OAM[i + 2]; //attribute 
             tempOAM[i + 3] = temp % 8; //set X attribute
             //spritesToDraw.push(i);
         }
@@ -174,19 +158,13 @@ $(document).ready(function() {
                 spriteY = tempOAM[spritesToDraw[i]];
                 tileNum = tempOAM[spritesToDraw[i] + 1];
                 spriteAttr = tempOAM[spritesToDraw[i] + 2];
-                if (PPU.spritePatTblAddr == 'left') {
-                    tile = PPU.CHRGrid[tileNum];
-                }
-                else if (PPU.spritePatTblAddr == 'right') {
-                    tile = PPU.BGRCHRGrid[tileNum];
-                }
-                // tile = PPU.spritePatTblAddr[tileNum];
+                tile = this.Mapper.getCHRGrid(this.PPU.spritePatTblAddr, tileNum);
                 tileRow = tile[curY - spriteY * 8];
                 paletteNum = spriteAttr & 0b00000011;
                 for (var x = 0; x < 8; x++) {
                     pixelColorIndex = tileRow[x];
-                    pixelColor = PPU.paletteColors[PPU.palette[16 + paletteNum * 4 + pixelColorIndex]];
-                    oamDisplay.updateBuffer((spriteX * 8) + x + (curY * 64), pixelColor);
+                    pixelColor = this.PPU.paletteColors[this.PPU.palette[16 + paletteNum * 4 + pixelColorIndex]];
+                    this.oamDisplay.updateBuffer((spriteX * 8) + x + (curY * 64), pixelColor);
                 }
             }
         }
@@ -194,9 +172,9 @@ $(document).ready(function() {
 
     var renderFrame = function() {
         if (!isPaused) {
-            CPU.runFrame();
-            CPU.totalCPUCyclesThisFrame;
-            CPU.nmiLoopCounter = 0;
+            this.CPU.runFrame();
+            this.CPU.totalCPUCyclesThisFrame;
+            this.CPU.nmiLoopCounter = 0;
             renderScreen();
         }
         requestAnimationFrame(renderFrame);
@@ -230,11 +208,11 @@ $(document).ready(function() {
         isPaused = !isPaused;
     });
     $('#reset').click(function() {
-        mainDisplay = null;
-        PPU = null;
-        MMU = null;
-        CPU = null;
-        APU = null;
+        this.mainDisplay = null;
+        this.PPU = null;
+        this.MMU = null;
+        this.CPU = null;
+        this.APU = null;
     });
     $('#btnSearchRoms').click(function() {
         var url = "/api/getRomListByName/" + $('#inpSearchRoms').val();
@@ -277,31 +255,31 @@ $(document).ready(function() {
         switch (key) {
             //Start
             case 17:
-                MMU.startBtnState = 1;
+                this.MMU.startBtnState = 1;
                 break;
             //A
             case 90:
-                MMU.aBtnState = 1;
+                this.MMU.aBtnState = 1;
                 break;
             //B
             case 88:
-                MMU.bBtnState = 1;
+                this.MMU.bBtnState = 1;
                 break;
             //Up
             case 38:
-                MMU.upBtnState = 1;
+                this.MMU.upBtnState = 1;
                 break;
             //Down
             case 40:
-                MMU.downBtnState = 1;
+                this.MMU.downBtnState = 1;
                 break;
              //Left
             case 37:
-                MMU.leftBtnState = 1;
+                this.MMU.leftBtnState = 1;
                 break;
              //Right
             case 39:
-                MMU.rightBtnState = 1;
+                this.MMU.rightBtnState = 1;
                 break;
         }
     });
@@ -313,31 +291,31 @@ $(document).ready(function() {
         switch (key) {
             //Start
             case 17:
-                MMU.startBtnState = 0;
+                this.MMU.startBtnState = 0;
                 break;
             //A
             case 90:
-                MMU.aBtnState = 0;
+                this.MMU.aBtnState = 0;
                 break;
             //B
             case 88:
-                MMU.bBtnState = 0;
+                this.MMU.bBtnState = 0;
                 break;
             //Up
             case 38:
-                MMU.upBtnState = 0;
+                this.MMU.upBtnState = 0;
                 break;
             //Down
             case 40:
-                MMU.downBtnState = 0;
+                this.MMU.downBtnState = 0;
                 break;
             //Left
             case 37:
-                MMU.leftBtnState = 0;
+                this.MMU.leftBtnState = 0;
                 break;
             //Right
             case 39:
-                MMU.rightBtnState = 0;
+                this.MMU.rightBtnState = 0;
                 break;
         }
     });
